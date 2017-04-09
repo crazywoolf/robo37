8186d04960168d2a421e747b8de7823fd5a9a4b5
crazywoolf <crazywoolf@mail.ru>
2017-04-09 22:45:50 +0300

    Добавили html отображение

---
+++ Robo37/WebUI/Controllers/CoursesController.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private ICourseRepository repository;
+        public CoursesController(ICourseRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Courses);
+        }
+    }
+}
