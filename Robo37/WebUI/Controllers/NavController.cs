30d0a1c40dff1bba0e51f8f3f7214bf304fc5085
crazywoolf <crazywoolf@mail.ru>
2017-04-17 00:17:17 +0300

    Добили навигацию

---
+++ Robo37/WebUI/Controllers/NavController.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        private ICourseRepository repository;
+
+        public NavController(ICourseRepository repo)
+        {
+            repository = repo;
+        }
+
+        public PartialViewResult Menu(string genre = null)
+        {
+            ViewBag.SelectedGenre = genre;
+
+            IEnumerable<string> genres = repository.Courses
+                .Select(course => course.Genre)
+                .Distinct()
+                .OrderBy(x => x);
+
+            return PartialView(genres);
+        }
+    }
+}
